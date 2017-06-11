ce1dbb5e7a3082e360158b08171fa10492ee2644
Maciej Abramowski <m.abramowski@wp.pl>
2017-06-11 09:57:03 +0200

    pobieranie elementow

---
+++ js/znajdywanieelementow.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var parFirst = document.getElementById('parFirst');
+console.log(parFirst);
+
+var elementsTable = document.getElementsByClassName('superlink');
+console.log(elementsTable);
+
+var allP = document.getElementsByTagName('a');
+console.log(allP);
+
+var allLink = document.querySelectorAll('.link');
+console.log(allLink);
+
+var firstLink = document.querySelector('.superlink');
+console.log(firstLink);
